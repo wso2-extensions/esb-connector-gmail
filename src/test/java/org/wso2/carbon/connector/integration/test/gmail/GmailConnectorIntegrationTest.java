f05a687e2a3a4455f261d57151ded7df258483fa
Hariprasath <tharis63@gmail.com>
2016-06-17 16:28:49 +0530

    Minor Changes

--- src/test/java/org/wso2/carbon/connector/integration/test/gmail/GmailConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/gmail/GmailConnectorIntegrationTest.java
@@ -40,7 +40,7 @@ public class GmailConnectorIntegrationTest extends ConnectorIntegrationTestBase 
      */
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("gmail-connector-3.0.2-SNAPSHOT");
+        init("gmail-connector-3.0.1");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
 
